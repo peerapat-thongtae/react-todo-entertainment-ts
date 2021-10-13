e8d1295fc572bf4024977561b198eafdf7c07b2e
peerapat-thongtae <peera.thongtae@gmail.com>
2021-10-13 13:54:25 +0700

    Movieslider get my movie list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,33 @@
 import Layout from 'components/Layout';
 import MovieSlider from 'components/MovieSlider';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import TodoService from 'services/TodoService';
 
 const Home = () => {
+  const [watchlistMovies, setWatchlistMovies] = useState([]);
+  const [watchedMovies, setWatchedMovies] = useState([]);
+  useEffect(() => {
+    TodoService.getMyMovieWatchlist().then((res) => {
+      setWatchlistMovies(res);
+    });
+
+    TodoService.getMyMovieWatched().then((res) => {
+      setWatchedMovies(res);
+    });
+  }, []);
   return (
     <Layout>
       <MovieSlider
-        title="Popular"
-        type="popular"
-        movies={[]}
+        title="My Movie Watchlist"
+        endpoint="/todo/movie/watchlist"
+        movies={watchlistMovies}
+        mediaType="movie"
+      />
+
+      <MovieSlider
+        title="My Movie Watched"
+        endpoint="/todo/movie/watched"
+        movies={watchedMovies}
         mediaType="movie"
       />
     </Layout>
--- src/services/TodoService.tsx
+++ src/services/TodoService.tsx
@@ -1,4 +1,5 @@
 import { ApiTodoHelper } from 'utils/APIHelper';
+import MovieService from './MovieService';
 
 const TodoService = {
   addMediaTodo: async (mediaData: any) => {
@@ -11,6 +12,40 @@ const TodoService = {
       return res.data;
     } catch (err) {
       return err.response.data;
+    }
+  },
+
+  getMyMovieWatchlist: async () => {
+    try {
+      const res = await ApiTodoHelper({
+        method: 'GET',
+        url: `/todo/mywatchlist/movie`,
+      });
+      const medias = Promise.all(
+        res.data.todos.map((todo: any) =>
+          MovieService.getMovieDetail(todo.mediaId)
+        )
+      );
+      return medias;
+    } catch (err) {
+      return err;
+    }
+  },
+
+  getMyMovieWatched: async () => {
+    try {
+      const res = await ApiTodoHelper({
+        method: 'GET',
+        url: `/todo/mywatched/movie`,
+      });
+      const medias = Promise.all(
+        res.data.todos.map((todo: any) =>
+          MovieService.getMovieDetail(todo.mediaId)
+        )
+      );
+      return medias;
+    } catch (err) {
+      return err;
     }
   },
 };
