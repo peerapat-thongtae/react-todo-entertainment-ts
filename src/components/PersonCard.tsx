64785dc18d1523ca1739205e2b73ae9cb6addc6a
peerapat-thongtae <peera.thongtae@gmail.com>
2021-10-19 05:06:13 +0700

    person list & card

--- src/components/PersonCard.tsx
+++ src/components/PersonCard.tsx
@@ -37,7 +37,7 @@ const PersonCard = (props: any) => {
               Known for:{' '}
               <Link
                 to={`/
-                ${person.known_for[0].media_type / person.known_for[0].id}`}
+                ${person.known_for[0].media_type}/${person.known_for[0].id}`}
                 className="hover:text-orange-500"
               >
                 {person.known_for[0].name || person.known_for[0].title || '-'}
